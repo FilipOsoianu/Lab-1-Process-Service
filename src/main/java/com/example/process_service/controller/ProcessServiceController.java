02baf2187e6bfc9e11ad12911b67671a6e340868
Filip Osoianu <filip.osoianu@sens.media>
2020-10-19 17:38:17 +0300

    create docker

--- src/main/java/com/example/process_service/controller/ProcessServiceController.java
+++ src/main/java/com/example/process_service/controller/ProcessServiceController.java
@@ -1,43 +1,68 @@
 package com.example.process_service.controller;
 
 import com.example.process_service.services.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
 import java.util.Map;
-
 
 @RestController
 public class ProcessServiceController {
 
+    @Value("${user-service-url}")
+    private String userServiceApi;
+
+    @Value("${insurance-service-url}")
+    private String insuranceServiceApi;
+
     RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
-    RestService restService = new RestService(restTemplateBuilder);
 
+    @Autowired
+    RestService restService = new RestService(restTemplateBuilder, userServiceApi, insuranceServiceApi);
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public Object show(@PathVariable String id, @RequestHeader("priority") String priority) {
         int userId = Integer.parseInt(id);
         return restService.getUserForId(userId, priority);
     }
 
-    @PostMapping("/user")
+    @GetMapping("/user/{user_id}/insurances")
+    public Object getInsuranceFroId(@PathVariable String user_id) {
+
+        return restService.getInsurancesByUserId(user_id);
+    }
+
+    @GetMapping("/status")
+    public Object getStatus() {
+
+        return restService.getStatus();
+    }
+
+    @PostMapping("/user/{user_id}/insurances")
+    public Object createInsurance(@PathVariable String user_id, @RequestBody Map<String, String> body) {
+
+        return restService.createInsurance(user_id, body);
+    }
+
+    @PostMapping("/users")
     public Object createUser(@RequestBody Map<String, String> body, @RequestHeader("priority") String priority) {
         return restService.createUser(body, priority);
     }
 
-    @PutMapping("/user/{id}")
+    @PutMapping("/users/{id}")
     public Object updateUser(@PathVariable String id, @RequestBody Map<String, String> body, @RequestHeader("priority") String priority) {
         return restService.updateUser(body, id, priority);
     }
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     public Object show(@RequestHeader("priority") String priority) {
         return restService.getUsers(priority);
     }
 
 
-    @DeleteMapping("user/{id}")
+    @DeleteMapping("users/{id}")
     public void deleteUser(@PathVariable String id, @RequestHeader("priority") String priority) {
         restService.deleteUser(id, priority);
     }
--- src/main/java/com/example/process_service/services/RestService.java
+++ src/main/java/com/example/process_service/services/RestService.java
@@ -1,45 +1,99 @@
 package com.example.process_service.services;
 
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
 
 @Service
 public class RestService {
 
     private final RestTemplate restTemplate;
 
-    public RestService(RestTemplateBuilder restTemplateBuilder) {
+    public final String userServiceUrl;
+    public final String insuranceServiceUrl;
+
+    @Autowired
+    public RestService(RestTemplateBuilder restTemplateBuilder, @Value("${user-service-url}") String userServiceUrl, @Value("${user-service-url}") String insuranceServiceUrl) {
         this.restTemplate = restTemplateBuilder.build();
+        this.userServiceUrl = userServiceUrl;
+        this.insuranceServiceUrl = insuranceServiceUrl;
     }
 
     public Object getUsers(String priority) {
-        String url = "http://localhost:8080/user";
+        String url = this.userServiceUrl + "/users";
         HttpHeaders headers = new HttpHeaders();
         headers.add("priority", priority);
         HttpEntity<Object> entity = new HttpEntity<Object>("", headers);
         return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
     }
+
+
+    public Object getStatus() {
+        String url = this.insuranceServiceUrl + "/working";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Object> entity = new HttpEntity<Object>("", headers);
+        return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+    }
+
+
+    public Object getInsurancesByUserId(String id) {
+        String url = this.userServiceUrl + "/users/" + id + "/insurances";
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Object> entity = new HttpEntity<Object>("", headers);
+        return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+    }
+
+    public Object createInsurance(String userId, Map<String, String> body) {
+        String url = this.userServiceUrl + "/insurance/create";
+        HttpHeaders headers = new HttpHeaders();
+
+        String price = body.get("price");
+        String name = body.get("name");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user_id", userId);
+        HttpEntity<Object> entity = new HttpEntity<>(jsonObject.toString(), headers);
+
+        this.restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
+
+//   jsonObject.put();
+
+        return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+    }
+
 
     public Object getUserForId(int id, String priority) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("priority", priority);
         HttpEntity<Object> entity = new HttpEntity<Object>("", headers);
-        String url = "http://localhost:8080/user/" + id;
-        return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+        String url = userServiceUrl + "users/" + id;
+        ResponseEntity<String> response = this.restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        if (response.getBody() == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        } else {
+            return this.restTemplate.exchange(url, HttpMethod.GET, entity, Object.class);
+        }
     }
 
     public Object createUser(Object object, String priority) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("priority", priority);
-        String url = "http://localhost:8080/user";
+        String url = userServiceUrl + "/users";
         HttpEntity<Object> entity = new HttpEntity<>(object, headers);
         return this.restTemplate.exchange(url, HttpMethod.POST, entity, Object.class);
     }
 
     public Object updateUser(Object object, String id, String priority) {
-        String url = "http://localhost:8080/user/" + id;
+        String url = userServiceUrl + "/users/" + id;
         HttpHeaders headers = new HttpHeaders();
         headers.add("priority", priority);
         HttpEntity<Object> entity = new HttpEntity<>(object, headers);
@@ -47,7 +101,7 @@ public class RestService {
     }
 
     public void deleteUser(String id, String priority) {
-        String url = "http://localhost:8080/user/" + id;
+        String url = userServiceUrl + "/users/" + id;
         HttpHeaders headers = new HttpHeaders();
         headers.add("priority", priority);
         HttpEntity<Object> entity = new HttpEntity<>("", headers);
